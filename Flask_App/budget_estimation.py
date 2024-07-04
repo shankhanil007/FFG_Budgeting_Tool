ab36e4e854c285aaf3257089500c1032d2684db8
Shankhanil Borthakur <shankhanilborthakur@gmail.com>
2024-07-05 01:37:10 +0530

    added new logic system

--- Flask_App/budget_estimation.py
+++ Flask_App/budget_estimation.py
@@ -18,6 +18,13 @@ def load_sql_config_file(location):
     module = importlib.import_module(module_name)
     return module.SQL_CONFIG
 
+def replace_norm_constants(sql, norm_constants):
+    for key, value in norm_constants.items():
+        # Use regex to find and replace the variables in the SQL
+        sql = re.sub(r'\{' + key + r'\}', str(value), sql)
+    return sql
+
+
 def replace_sql_placeholders(sql_query, constants):
     # Regular expression to find placeholders in the format {variable}
     placeholder_pattern = r'\{(\w+)\}'
@@ -46,7 +53,9 @@ def calculate_budget(database="bangalore", schema="basawanagudi_210"):
     # load config files
     RATE_CONFIG = load_rate_config_file(database + "_" + schema)
     SQL_CONFIG = load_sql_config_file(database + "_" + schema)
-    
+
+
+
     for parent_catergory, child_catergory in six.iteritems(RATE_CONFIG):
         for child_catergory, dict in six.iteritems(RATE_CONFIG.get(parent_catergory)):
 
@@ -82,6 +91,9 @@ def calculate_budget(database="bangalore", schema="basawanagudi_210"):
             base_sql = base_sql[:-1]
             base_sql = base_sql.format(SCHEMA=schema)
 
+            if parent_sql_config == "ROAD_ID_PARKING_MARKING_2W":
+                print(base_sql)
+
             cost_calculation_sql = """  SELECT
                                             CAST(
                                                 REGEXP_REPLACE(FOOTPATH_SIDE, '[^0-9]', '', 'g') AS INTEGER
@@ -101,6 +113,58 @@ def calculate_budget(database="bangalore", schema="basawanagudi_210"):
             df = pd.read_sql_query(final_sql_query, conn)
             df.to_csv(csv_path, index=False)
 
+    # close the cursor and connection 
+    cur.close()  
+    conn.close()
+
+
+
+def calculate_budget_new(database="bangalore", schema="basawanagudi_110"):
+    """ function to calculate budget for a ward """
+
+    # connect to the database
+    user = os.environ.get("USER")
+    password = os.environ.get("PASSWORD")
+    host = os.environ.get("HOST")
+    port = os.environ.get("PORT")
+    conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port) 
+    
+    # create a cursor 
+    cur = conn.cursor() 
+
+    # load config files
+    RATE_CONFIG = load_rate_config_file(database + "_" + schema)
+    SQL_CONFIG = load_sql_config_file(database + "_" + schema)
+
+    merged_df = pd.DataFrame()
+
+    for parent_catergory, child_catergory in six.iteritems(RATE_CONFIG):
+        for child_catergory, dict in six.iteritems(RATE_CONFIG.get(parent_catergory)):
+            rate = dict.get('RATE')
+            norm_constants = dict.get("NORM_CONSTANTS")
+            sql_template = SQL_CONFIG.get(child_catergory)
+            if norm_constants:
+                # Replace the constants in the SQL template
+                sql_template = replace_norm_constants(sql_template, norm_constants)
+
+            cost_calculation_sql = """  SELECT
+                                            *,
+                                            COALESCE(BUDGET_CALCULATION_QUANTITY::numeric, 0) * {} AS {}
+                                        FROM
+                                            ({}) AS SQl_QUANTITY
+                                        """.format(rate, child_catergory, sql_template)
+            
+            final_sql_query = cost_calculation_sql
+            df = pd.read_sql_query(final_sql_query, conn)
+            filtered_df = df[['footpath_side', '{}'.format(child_catergory.lower())]]
+            if len(merged_df) == 0:
+                merged_df = filtered_df
+            else:
+                merged_df = pd.merge(merged_df, filtered_df, on='footpath_side', how='left')
+
+    csv_path = "./results/cost_estimate.csv"
+    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+    merged_df.to_csv(csv_path, index=False)
     # close the cursor and connection 
     cur.close()  
     conn.close()
